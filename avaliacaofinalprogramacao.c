2f2d459f1873c0f5b6696e46046bd6321353155a
keth05 <kethelenbitencourt276@gmail.com>
2024-08-22 14:20:18 -0300

    commit todas as questoes

---
+++ avaliacaofinalprogramacao.c
@@ -0,0 +1,37 @@
+1) Qual biblioteca é necessário incluir com diretiva #include para atualizar as funções de entrada padrão da linguagem c?
+
+  //("é necessário por abaixo de "#include" <stdio.h> para atualizar as funcões")
+ 
+2) O trecho de código realiza a leitura de um numéro inteiro Entretando, ele contém dois erros. Corrija-os e justifique sua reposta.
+          int n;
+          scanf("%f",n);
+   //("int num;
+     scanf("%f", &num); ")
+
+3)Explique o trecho de código abaixo (linha 2)
+          char c;
+          scanf("%c", &c);
+          while(getchar c) != 'ln');
+   //("serve para traduzir ou modificar as variáveis em linguagem em C");
+
+4) Explique o trecho de código abaixo
+          float largura, comprimeto,
+          scanf ("%f%f", &largura, de comprimento);
+//("tem como função traduzir uma variável float, por isso o f, e & para modificar o endereço de memoria de uma variavel");
+
+5) Explique o trecho de código abaixo
+         char nome [80]
+         scanf("%s", nome);
+//("traduzir e armazenar valores inteiros de um menbro do conjunto de caracteres representaveis")
+6) Explique o trecho de código abaixo
+         printf("Olá, mundo!");
+//(" funcao de imprimir uma mensagem na tela");
+
+7) Explique o trecho de código abaixo. Dica a resposta nâo é a mesma da quetão anterior de código abaixo
+         printf("Olá, mundo!\n");
+
+//(" exibir um ou mais dados na tela");
+
+8) Explique o trecho de código abaixo 
+         printf("1+1\n");
+  //("exibir resultados de calculos");
